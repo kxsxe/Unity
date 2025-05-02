4d1723b12ca1eafea036d856f1a0d7736e53c3c8
kxsxe <20234339@s.scnu.ac.kr>
2025-05-03 03:47:23 +0900

    Ch06

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+    float threshold = 0.2f;
 
     void Start()
     {
         Application.targetFrameRate = 60;
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
-    public void LButtonDown()
+    void Update()
     {
-        transform.Translate(-3, 0, 0);
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
+        {
+            this.animator.SetTrigger("JumpTrigger");
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * walkForce);
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        if (this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 
-     public void RButtonDown()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        transform.Translate(3, 0, 0);
+        Debug.Log("°ñ");
+        SceneManager.LoadScene("ClearScene");
     }
 }
